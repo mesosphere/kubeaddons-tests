82e7a65d12cd2a92715499b56bfd50eaf1fd10b6
Zain Malik <zmalikshxil@gmail.com>
2020-06-12 17:33:19 +0200

    adapt the script for kubeaddons-enterprise also

--- run-tests.sh
+++ run-tests.sh
@@ -3,12 +3,16 @@
 
 TESTING_BRANCH=${TESTING_BRANCH:-dev}
 KUBEADDONS_ENTERPRISE_PATH=${KUBEADDONS_ENTERPRISE_PATH:-kubeaddons-enterprise}
+KUBEADDONS_ENTERPRISE_TESTS_PATH="./tests"
+KUTTL_TEST_CONFIGURATION_PATH="kuttl-test.yaml"
 printf "testing kubeaddons-enterprise branch %s\n" "$TESTING_BRANCH"
 
-if [ -d "../addons" ]
+if [ -d "./addons" ]
 then
   printf "Running inside the kubeaddons-enterprise repository"
-  KUBEADDONS_ENTERPRISE_PATH="../."
+  KUBEADDONS_ENTERPRISE_PATH="."
+  KUBEADDONS_ENTERPRISE_TESTS_PATH="kubeaddons-enterprise-tests/tests/kubeaddons-enterprise"
+  KUTTL_TEST_CONFIGURATION_PATH="kubeaddons-enterprise-tests/kuttl-test.yaml"
 elif [ -d "$KUBEADDONS_ENTERPRISE_PATH" ]
 then
   pushd $KUBEADDONS_ENTERPRISE_PATH
@@ -23,9 +27,9 @@ KUBEADDONS_ENTERPRISE_ABS_PATH=$(realpath -L "${KUBEADDONS_ENTERPRISE_PATH}")
 for i in $(find "${KUBEADDONS_ENTERPRISE_PATH}"/addons -mindepth 2 -type d)
 do
     printf "addon directory detected: %s\n" "$i"
-    if [ ! -d "./tests/$i" ]
+    if [ ! -d "${KUBEADDONS_ENTERPRISE_TESTS_PATH}/$i" ]
     then
-      printf "missing tests for addon directory %s\n" "./tests/$i"
+      printf "missing tests for addon directory %s\n" "${KUBEADDONS_ENTERPRISE_TESTS_PATH}/$i"
       exit 1
     fi
 done
@@ -36,5 +40,5 @@ printf "Path %s\n" "$KUBEADDONS_ENTERPRISE_ABS_PATH"
 
 for i in $(find "${KUBEADDONS_ENTERPRISE_PATH}"/addons -mindepth 2 -type d)
 do
-    KUBEADDONS_ENTERPRISE_ABS_PATH="$KUBEADDONS_ENTERPRISE_ABS_PATH" kubectl-kuttl test tests/"$i" | tee /dev/fd/2 | go-junit-report -set-exit-code > dist/${i////_}-addons_test_report.xml
+    KUBEADDONS_ENTERPRISE_ABS_PATH="$KUBEADDONS_ENTERPRISE_ABS_PATH" kubectl-kuttl test --config=${KUTTL_TEST_CONFIGURATION_PATH} ${KUBEADDONS_ENTERPRISE_TESTS_PATH}/$i | tee /dev/fd/2 | go-junit-report -set-exit-code > dist/${i////_}-addons_test_report.xml
 done
