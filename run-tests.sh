56a37b0c6f43c59fddd9869274aeeac950de8641
Zain Malik <zmalikshxil@gmail.com>
2020-06-15 10:48:56 +0200

    Apply suggestions from code review
    Co-authored-by: Marcin Owsiany <mowsiany@D2iQ.com>

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -euo pipefail
 
 TESTING_BRANCH=${TESTING_BRANCH:-dev}
 KUBEADDONS_ENTERPRISE_PATH=${KUBEADDONS_ENTERPRISE_PATH:-kubeaddons-enterprise}
@@ -15,16 +15,14 @@ then
   KUTTL_TEST_CONFIGURATION_PATH="kubeaddons-enterprise-tests/kuttl-test.yaml"
 elif [ -d "$KUBEADDONS_ENTERPRISE_PATH" ]
 then
-  pushd $KUBEADDONS_ENTERPRISE_PATH
-  git checkout ${TESTING_BRANCH}
-  popd
+  git -C "${KUBEADDONS_ENTERPRISE_PATH}" checkout ${TESTING_BRANCH}
 else
   git clone --depth 1 https://github.com/mesosphere/kubeaddons-enterprise.git --branch ${TESTING_BRANCH} --single-branch
 fi
 
 KUBEADDONS_ENTERPRISE_ABS_PATH=$(realpath -L "${KUBEADDONS_ENTERPRISE_PATH}")
 
-for i in $(find "${KUBEADDONS_ENTERPRISE_PATH}"/addons -mindepth 2 -type d)
+for i in $(find "${KUBEADDONS_ENTERPRISE_PATH}"/addons -mindepth 2 -maxdepth 2 -type d)
 do
     printf "addon directory detected: %s\n" "$i"
     if [ ! -d "${KUBEADDONS_ENTERPRISE_TESTS_PATH}/$i" ]
@@ -38,7 +36,7 @@ go get github.com/jstemmer/go-junit-report
 mkdir -p dist
 printf "Path %s\n" "$KUBEADDONS_ENTERPRISE_ABS_PATH"
 
-for i in $(find "${KUBEADDONS_ENTERPRISE_PATH}"/addons -mindepth 2 -type d)
+for i in $(find "${KUBEADDONS_ENTERPRISE_PATH}"/addons -mindepth 2 -maxdepth 2 -type d)
 do
     KUBEADDONS_ENTERPRISE_ABS_PATH="$KUBEADDONS_ENTERPRISE_ABS_PATH" kubectl-kuttl test --config=${KUTTL_TEST_CONFIGURATION_PATH} ${KUBEADDONS_ENTERPRISE_TESTS_PATH}/$i | tee /dev/fd/2 | go-junit-report -set-exit-code > dist/${i////_}-addons_test_report.xml
 done
