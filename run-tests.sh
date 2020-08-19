a87f960de82f03f5e9e58ebf540d5141560fe5da
Joe Julian <me@joejulian.name>
2020-08-19 12:40:52 -0700

    fix tests path when run in repo

--- run-tests.sh
+++ run-tests.sh
@@ -10,17 +10,18 @@ printf "testing %s branch %s\n" "${KUBEADDONS_REPO}" "${TESTING_BRANCH}"
 if [ -d "./addons" ]
 then
   printf "Running inside the %s repository\n" "${KUBEADDONS_REPO}"
+  KUBEADDONS_TESTS_PATH="${KUBEADDONS_TESTS_PATH}/${KUBEADDONS_REPO}"
   KUBEADDONS_REPO="."
 elif [ -d "${KUBEADDONS_REPO}" ]
 then
-  git -C "${KUBEADDONS_REPO}" checkout ${TESTING_BRANCH}
+  git -C "${KUBEADDONS_REPO}" checkout "${TESTING_BRANCH}"
 else
-  git clone --depth 1 https://github.com/mesosphere/${KUBEADDONS_REPO}.git --branch ${TESTING_BRANCH} --single-branch
+  git clone --depth 1 "https://github.com/mesosphere/${KUBEADDONS_REPO}.git" --branch "${TESTING_BRANCH}" --single-branch
 fi
 
 KUBEADDONS_ABS_PATH=$(realpath -L "${KUBEADDONS_REPO}")
 
-for i in $(find "${KUBEADDONS_REPO}"/addons -mindepth 2 -maxdepth 2 -type d)
+for i in $(find "${KUBEADDONS_REPO}/addons" -mindepth 2 -maxdepth 2 -type d)
 do
     printf "addon directory detected: %s\n" "${i}"
     if [ ! -d "${KUBEADDONS_TESTS_PATH}/${i}" ]
@@ -31,9 +32,13 @@ do
 done
 
 printf "Path %s\n" "${KUBEADDONS_ABS_PATH}"
-for i in $(find "${KUBEADDONS_REPO}"/addons -mindepth 2 -maxdepth 2 -type d)
+for i in $(find "${KUBEADDONS_REPO}/addons" -mindepth 2 -maxdepth 2 -type d)
 do
     printf "running tests in %s\n" "${KUBEADDONS_TESTS_PATH}/${i}"
-    mkdir -p dist/${i////_}
-    KUBEADDONS_ABS_PATH="${KUBEADDONS_ABS_PATH}" kubectl-kuttl test --artifacts-dir=./dist/${i////_} --config=${KUTTL_TEST_CONFIGURATION_PATH} ${KUBEADDONS_TESTS_PATH}/${i} --report xml
+    mkdir -p "dist/${i////_}"
+    KUBEADDONS_ABS_PATH="${KUBEADDONS_ABS_PATH}" kubectl-kuttl test \
+        --artifacts-dir="./dist/${i////_}" \
+        --config="${KUTTL_TEST_CONFIGURATION_PATH}" \
+        --report xml \
+        "${KUBEADDONS_TESTS_PATH}/${i}"
 done
