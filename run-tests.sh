6d9143f1d1b2b0ef9f133782376c9580efb1e4fb
Joe Julian <me@joejulian.name>
2020-08-19 15:28:31 -0700

    don't use exported KUBECONFIG

--- run-tests.sh
+++ run-tests.sh
@@ -3,8 +3,24 @@ set -euo pipefail
 
 TESTING_BRANCH=${TESTING_BRANCH:-dev}
 KUBEADDONS_REPO=${KUBEADDONS_REPO:-kubeaddons-enterprise}
-KUBEADDONS_TESTS_PATH="$(dirname $0)/tests"
+KUBEADDONS_TESTS_PATH="$(dirname ${0})/tests"
 KUTTL_TEST_CONFIGURATION_PATH="${KUBEADDONS_TESTS_PATH}/${KUBEADDONS_REPO}/kuttl-test.yaml"
+
+run_test() {
+    local ARTIFACT="dist/${1////_}"
+    local KUBECONFIG="${KUBEADDONS_TEST_KUBECONFIG}" 
+    local TEST="${KUBEADDONS_TESTS_PATH}/${1}"
+    export ARTIFACT KUBECONFIG TEST
+
+    printf "running tests in %s\n" "${TEST}"
+    mkdir -p "${ARTIFACT}"
+    kubectl-kuttl test \
+        --artifacts-dir="${ARTIFACT}" \
+        --config="${KUTTL_TEST_CONFIGURATION_PATH}" \
+        --report xml \
+        "${TEST}"
+}
+
 printf "testing %s branch %s\n" "${KUBEADDONS_REPO}" "${TESTING_BRANCH}"
 
 if [ -d "./addons" ]
@@ -16,12 +32,20 @@ elif [ -d "${KUBEADDONS_REPO}" ]
 then
   git -C "${KUBEADDONS_REPO}" checkout "${TESTING_BRANCH}"
 else
-  git clone --depth 1 "https://github.com/mesosphere/${KUBEADDONS_REPO}.git" --branch "${TESTING_BRANCH}" --single-branch
+  git clone "https://github.com/mesosphere/${KUBEADDONS_REPO}.git" \
+      --depth 1 \
+      --branch "${TESTING_BRANCH}" \
+      --single-branch
 fi
 
-KUBEADDONS_ABS_PATH=$(realpath -L "${KUBEADDONS_REPO}")
+ADDON_LIST=()
+while IFS=  read -r -d $'\0'; do
+    ADDON_LIST+=("$REPLY")
+done < <(find "${KUBEADDONS_REPO}/addons" -mindepth 2 -maxdepth 2 -type d -print0)
 
-for i in $(find "${KUBEADDONS_REPO}/addons" -mindepth 2 -maxdepth 2 -type d)
+export KUBEADDONS_ABS_PATH=$(realpath -L "${KUBEADDONS_REPO}")
+
+for i in ${ADDON_LIST[*]}
 do
     printf "addon directory detected: %s\n" "${i}"
     if [ ! -d "${KUBEADDONS_TESTS_PATH}/${i}" ]
@@ -32,13 +56,7 @@ do
 done
 
 printf "Path %s\n" "${KUBEADDONS_ABS_PATH}"
-for i in $(find "${KUBEADDONS_REPO}/addons" -mindepth 2 -maxdepth 2 -type d)
+for i in ${ADDON_LIST[*]}
 do
-    printf "running tests in %s\n" "${KUBEADDONS_TESTS_PATH}/${i}"
-    mkdir -p "dist/${i////_}"
-    KUBEADDONS_ABS_PATH="${KUBEADDONS_ABS_PATH}" kubectl-kuttl test \
-        --artifacts-dir="./dist/${i////_}" \
-        --config="${KUTTL_TEST_CONFIGURATION_PATH}" \
-        --report xml \
-        "${KUBEADDONS_TESTS_PATH}/${i}"
+    run_test "$i"
 done
