90e7501306c77c5623cf0af3fadacbe235d28daa
Joe Julian <me@joejulian.name>
2020-08-25 16:21:52 -0700

    add kubernetes-base-addons test for traefik (#7)
    * add test for traefik
    
    * fix yamllint errors

---
+++ tests/kubernetes-base-addons/addons/traefik/1.7.x/traefik-spdy/metallb-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+JP='{.status.addresses[?(.type == "InternalIP")].address}'
+NODEIP=$(kubectl get node kind-control-plane -o jsonpath="${JP}")
+SLASH24="${NODEIP%.*}"
+kubectl -n kubeaddons create configmap metallb-config \
+    --from-literal=config='{"address-pools":[{"name":"default","protocol":"layer2","addresses":["'"${SLASH24}"'.200-'"${SLASH24}"'.250"]}]}' \
+    --dry-run -o yaml | kubectl apply -f -
